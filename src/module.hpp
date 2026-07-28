46f45131d6c9373e37369f3ba84e599c36368983
Ink-exp <marconfff@gmail.com>
2026-07-28 20:27:33 +0800

    Update module.hpp

--- src/module.hpp
+++ src/module.hpp
@@ -1,1 +1,12 @@
+#pragma once
+#include <string>
+
+class IModule {
+public:
+    virtual ~IModule() = default;
+    virtual std::string get_name() const = 0;
+    virtual void on_enable() = 0;
+    virtual void on_disable() = 0;
+    virtual void on_render() {}
+};
 
