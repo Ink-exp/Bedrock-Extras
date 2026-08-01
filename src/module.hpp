9f090037f12e6c1bedb386610ab7f2513cccc08c
Ink-exp <marconfff@gmail.com>
2026-08-01 14:29:41 +0800

    Update module.hpp

--- src/module.hpp
+++ src/module.hpp
@@ -5,8 +5,8 @@ class IModule {
 public:
     virtual ~IModule() = default;
     virtual std::string get_name() const = 0;
+    virtual bool is_enabled() const = 0;
+    virtual void set_enabled(bool state) = 0;
     virtual void on_enable() = 0;
     virtual void on_disable() = 0;
-    virtual void on_render() {}
 };
-
